0b2615d8cd2f849b8c5a9d4c620d58756202716c
seyoung1007 <bobb7771@gmail.com>
2026-04-22 16:01:53 +0900

    이벤트 key up , 이벤트 예제 , 이벤트 컬러 탭 공부

---
+++ event/js/event-detail.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+  // console.log("hi");
+  const open = document.querySelector("#open");
+  const detail = document.querySelector("#detail");
+  const close = document.querySelector("#close");
+  open.addEventListener("click", () => {
+    detail.style.display = "block";
+    open.style.display = "none";
+  });
+  close.addEventListener("click", () => {
+    detail.style.display = "none";
+    open.style.display = "block";
+  })
+});
